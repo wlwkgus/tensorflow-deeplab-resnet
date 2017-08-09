a5b3560a8dfefc3052b9ca4deb8b24444626507c
Tony Kim <tony.kim@bucketplace.net>
2017-08-09 15:58:57 +0900

    finally done

--- bulk_inference.py
+++ bulk_inference.py
@@ -70,8 +70,10 @@ def main():
     imgs = None
     shapes = []
     print("Loading...")
-    for fname in listdir(args.img_path):
+    for fname in sorted(listdir(args.img_path)):
         print(fname)
+        if fname.startswith('.'):
+            continue
         # Prepare image.
         if fname.split('.')[1] in ('jpg', 'jpeg', 'JPG'):
             img = tf.image.decode_jpeg(tf.read_file(args.img_path + fname), channels=3)
